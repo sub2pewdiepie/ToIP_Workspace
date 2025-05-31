5a51ce599fa9e716051290d9bd725ba81debb202
7oxotsist <proninpv2304@gmail.com>
2025-05-31 20:10:18 +0000

    Обновил скрипт для гена .env

--- gen_env.sh
+++ gen_env.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 PASSWORD=$(jq -r '.password' ./config/config.json)
+DBNAME=$(jq -r '.dbname' ./config/config.json)
+USERNAME=$(jq -r '.user' ./config/config.json)
 
 echo "POSTGRES_PASSWORD=$PASSWORD" > .env
 echo "DOMAIN=4edu.su" >> .env
 echo "EMAIL=proninpv2304@gmail.com" >> .env
+echo "POSTGRES_USER=$USERNAME" >> .env
+echo "POSTGRES_DB=$DBNAME" >> .env
+echo "LOG_FORMAT=text" >> .env
+echo "LOG_LEVEL=" >> .env
