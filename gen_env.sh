4de90925a791737bf4b61659100595572069db1b
ivanvasutin <ivanvakk@yandex.ru>
2025-06-01 14:42:23 +0300

    теперь всё лежит в секретах

--- gen_env.sh
+++ gen_env.sh
@@ -4,10 +4,10 @@ PASSWORD=$(jq -r '.password' ./config/config.json)
 DBNAME=$(jq -r '.dbname' ./config/config.json)
 USERNAME=$(jq -r '.user' ./config/config.json)
 
-echo "POSTGRES_PASSWORD=$PASSWORD" > .env
-echo "DOMAIN=4edu.su" >> .env
-echo "EMAIL=proninpv2304@gmail.com" >> .env
-echo "POSTGRES_USER=$USERNAME" >> .env
-echo "POSTGRES_DB=$DBNAME" >> .env
-echo "LOG_FORMAT=text" >> .env
-echo "LOG_LEVEL=" >> .env
+# echo "POSTGRES_PASSWORD=$PASSWORD" > .env
+# echo "DOMAIN=4edu.su" >> .env
+# echo "EMAIL=proninpv2304@gmail.com" >> .env
+# echo "POSTGRES_USER=$USERNAME" >> .env
+# echo "POSTGRES_DB=$DBNAME" >> .env
+# echo "LOG_FORMAT=text" >> .env
+# echo "LOG_LEVEL=" >> .env
